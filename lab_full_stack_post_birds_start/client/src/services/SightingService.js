ba3524169f3be701bd4352430b6c0edcd5125d02
Aresky Berkane <areskyberkane@gmail.com>
2019-12-03 14:13:37 +0000

    added post request

--- lab_full_stack_post_birds_start/client/src/services/SightingService.js
+++ lab_full_stack_post_birds_start/client/src/services/SightingService.js
@@ -4,5 +4,13 @@ export default {
   getSightings(){
     return fetch(baseURL)
     .then(res => res.json())
+  },
+  sendBird(bird){
+    return fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify(bird),
+      header: {'content-type': 'application/json'}
+    })
+    .then(result => result.json())
   }
 }
