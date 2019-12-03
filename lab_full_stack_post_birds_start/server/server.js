639084d0c85586628a25f431066116b88a41f251
Aresky Berkane <areskyberkane@gmail.com>
2019-12-03 14:14:42 +0000

    looked into files

--- lab_full_stack_post_birds_start/server/server.js
+++ lab_full_stack_post_birds_start/server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 const cors = require('cors');
+const parser = require('body-parser');
+app.use(parser.json());
 app.use(cors());
 
 const MongoClient = require('mongodb').MongoClient;
